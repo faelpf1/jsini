f7f4974662bf2a4291e9edc7953a25bfc87abcd9
Rafael Filgueiras <rafaelpfilgueiras@gmail.com>
2020-08-15 18:24:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var x = 2;
+x=6;
+//console.log(x);
+
+const y = "a";
+//y="b"; Erro
+//console.log(y);
+
+
+//Variaveis Globais e Locais
+{
+  let z = 4;
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+//console.log(z);
